376ef9a6a0725ac8536115bb6259665754dd9a5f
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-04-12 23:21:30 +0300

    add horizontal scroll to stories container

--- src/components/Story/StoryContainer.js
+++ src/components/Story/StoryContainer.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import HorizontalScroll from 'react-scroll-horizontal'
 import Story from './Story'
 import './StoryContainer.css'
 import avatar from '../../assets/images/avatar.jpg'
@@ -8,12 +9,20 @@ const StoryBox = () => {
     { name: "yalperg", img: avatar, newStory: true },
     { name: "yalperg2", img: avatar, newStory: false },
     { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
+    { name: "yalperg3", img: avatar, newStory: false },
   ])
   return (
     <div className="story-container">
-      <ul>
-        {stories.map(story => <Story story={story} />)}
-      </ul>
+      <HorizontalScroll reverseScroll={true}>
+        <ul>
+          {stories.map(story => <Story story={story} />)}
+        </ul>
+      </HorizontalScroll>
     </div>
   )
 }
