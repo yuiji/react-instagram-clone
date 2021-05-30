590cfbc349edf69face2dbd87bfc3fdf5b515ccc
Yunus Alper Göl <yunus.alper.gl@gmail.com>
2021-05-30 17:19:23 +0300

    Add emoji menu to comment section.

--- src/components/Post/CommentInput.js
+++ src/components/Post/CommentInput.js
@@ -1,30 +1,49 @@
 import { useState, useContext } from 'react'
+import Picker from 'emoji-picker-react'
 import { PostContext } from '../../contexts/PostContext'
 import { UserContext } from '../../contexts/UserContext'
 import { ReactComponent as EmojiSvg } from '../../assets/icons/emoji.svg'
 import '../../styles/CommentInput.css'
 
-
 const CommentInput = ({ id }) => {
   const [comment, setComment] = useState('')
+  const [openMenu, setOpenMenu] = useState(false)
   const { addComment } = useContext(PostContext)
   const { user } = useContext(UserContext)
 
-  const submitHandler = (e) => {
+  const submitHandler = e => {
     e.preventDefault()
-    addComment(id, {name: user.username, comment: comment})
+    addComment(id, { name: user.username, comment: comment })
     setComment('')
   }
 
-  const inputChangeHandler = (e) => {
+  const inputChangeHandler = e => {
     setComment(e.target.value)
+  }
+
+  const onEmojiClick = (event, emojiObject) => {
+    setComment(comment + emojiObject.emoji)
+  }
+
+  const toggleEmojiMenu = () => {
+    setOpenMenu(!openMenu)
   }
 
   return (
     <form className='comment-input__container' onSubmit={submitHandler}>
-      <EmojiSvg />
-      <input type='text' placeholder='Add a comment...' value={comment} onChange={inputChangeHandler} />
-      <button>Post</button>
+      <EmojiSvg onClick={toggleEmojiMenu} />
+      <input
+        type='text'
+        placeholder='Add a comment...'
+        value={comment}
+        onChange={inputChangeHandler}
+      />
+      <button type='submit'>Post</button>
+      {openMenu && (
+        <div className='comment-input__emoji'>
+          <Picker onEmojiClick={onEmojiClick} />
+        </div>
+      )}
     </form>
   )
 }
